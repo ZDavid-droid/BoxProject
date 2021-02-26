028c8daaa454cd34b025dcc412a477dada05503a
didar <didarenator@gmail.com>
2021-02-26 17:43:05 +0600

    fist page

--- app/src/main/java/kz/krpt/boxproject/MainActivity.kt
+++ app/src/main/java/kz/krpt/boxproject/MainActivity.kt
@@ -2,14 +2,20 @@ package kz.krpt.boxproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 import kz.krpt.boxproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var currentPosition: Int = 0
     private lateinit var  navController: NavController
+    private lateinit var appBar: AppBarConfiguration
+    private lateinit var titles: Array<String>
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,5 +27,22 @@ class MainActivity : AppCompatActivity() {
         //val appBar = AppBarConfiguration(setOf(R.id.topFragment), drawer_layout)
         //toolbar.setupWithNavController(navController, appBar)
         navController = navHostFragment.navController
+        drawerMenu(binding.drawerNavigation)
     }
+    private fun drawerMenu(navigation: NavigationView){
+        navigation.setNavigationItemSelectedListener {p0 ->
+            when(p0.itemId){
+                R.id.main_item ->{
+                    currentPosition = 0
+                    if(navController.currentDestination!!.id != R.id.topFragment){
+                        navController.navigate(R.id.topFragment)
+                    }
+                }
+
+            }
+            drawer_layout.closeDrawer(GravityCompat.START)
+            true
+        }
+    }
+
 }
